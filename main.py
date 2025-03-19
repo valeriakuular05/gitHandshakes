88d1b9f713baa92d187d21deafc67fe0f5ba834b
valeria <vrkuular@gmail.com>
2025-03-20 01:11:01 +0300

    added argparse module
    enter on the command line:
    --input pathRepo --output name.json
    arguments are optional

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from libs.func import collect_data
+from libs.func import collect_data, startProg
 from libs.graph import gr, max_handshakes
+import json, os
 
 
-repo_path = str(input("Введите путь к репозиторию: "))
-# collect the data set
-data_dict = collect_data(repo_path)
-# creating a programmer graph
+res_of_start = startProg() # [--input, --output]
+if (res_of_start[0]):  #if --input repo --output name.json
+    # collect the data set
+    collect_data(res_of_start)
+
+# start the 2nd part of the program 
+with open(res_of_start[1], "r", encoding="utf-8") as file:
+    data_dict = json.load(file)
 G = gr(data_dict)
 # max lenth of handshakers between programmers
 max_handshakes(G)
